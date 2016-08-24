3961062abf05391dbc185256831f43d12f5823c2
wangsiyuan6 <wangsiyuan1@jd.com>
2016-08-24 20:57:49 +0800

    '更新数据库脚本'

--- doc/survey.sql
+++ doc/survey.sql
@@ -1,8 +1,8 @@
 -- --------------------------------------------------------
 -- 主机:                           127.0.0.1
--- 服务器版本:                        5.6.19-enterprise-commercial-advanced - MySQL Enterprise Server - Advanced Edition (Commercial)
+-- 服务器版本:                        5.7.12-log - MySQL Community Server (GPL)
 -- 服务器操作系统:                      Win64
--- HeidiSQL 版本:                  9.3.0.5045
+-- HeidiSQL 版本:                  9.3.0.5043
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -18,21 +18,32 @@ CREATE TABLE IF NOT EXISTS `survey_paper` (
   `start_time` datetime NOT NULL COMMENT '调查问卷开始时间',
   `end_time` datetime NOT NULL COMMENT '调查问卷结束时间',
   `is_publish` tinyint(3) unsigned NOT NULL COMMENT '是否发布 0未发布 1已发布',
+  `create_time` datetime NOT NULL,
+  `update_time` datetime NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='调查问卷表';
+
+-- 数据导出被取消选择。
+-- 导出  表 wen_juan.survey_paper_question 结构
+CREATE TABLE IF NOT EXISTS `survey_paper_question` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `paper_id` bigint(20) unsigned NOT NULL COMMENT 'survey_paper.id',
+  `question_id` bigint(20) unsigned NOT NULL COMMENT 'survey_question.id',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='问卷试题关联表';
 
 -- 数据导出被取消选择。
 -- 导出  表 wen_juan.survey_question 结构
 CREATE TABLE IF NOT EXISTS `survey_question` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `title` varchar(200) NOT NULL COMMENT '题干',
-  `mode` tinyint(4) NOT NULL COMMENT '0单选 1多选 2问答题',
+  `mode` tinyint(4) NOT NULL COMMENT '1单选 2多选 3问答题',
   `options` varchar(2000) DEFAULT NULL COMMENT '{order:1,option:"xx"} 问答题NULL',
   `is_del` tinyint(4) NOT NULL DEFAULT '0' COMMENT '0未删除 1已删除',
   `update_time` datetime NOT NULL,
   `create_time` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8 COMMENT='问卷试题表';
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8 COMMENT='问卷试题表';
 
 -- 数据导出被取消选择。
 -- 导出  表 wen_juan.survey_statistics 结构
